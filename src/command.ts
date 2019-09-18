0dd9eb6379bfd0a078ca90ca9c5598ad7c8dcf4e
mdugan <mdugan@protonmail.com>
2019-09-17 21:57:59 -0500

    added some photos

--- src/command.ts
+++ src/command.ts
@@ -1,19 +1,14 @@
 import { Message } from 'discord.js'
-import * as isUrl from 'is-url'
 
 import { client } from './client'
 import { getCoin, top10 } from './util/crypto'
 import { DAB } from './util/emoji'
-import { addMeme, getMeme } from './util/memes'
-import { learnPhrase } from './util/phrases'
 import { addQuote, getQuote } from './util/quotes'
 
 const HELP: string = "Here's what I can do:\n\n" 
   + "`\!yeet crypto` Prices out the top 10 cryptocurrencies, you can get more specific info on a currency by specifying a name.\n\n"
   + "`\!yeet dab` Uhh...\n\n"
   + "`\!yeet game` Set the bot's game\n\n"
-  + "`\!yeet learn` Learns a phrase and response for the next 24 hours.  The trigger phrase will be the first quoted string and the response will be the second.\n\n"
-  + "`\!yeet meme` Either posts or learns a meme from the vault, with credit to the original submitter.  Memes **must** be URL's.\n\n"
   + "`\!yeet nick` Change the bot's nickname on a given server.  You _need_ to give it the right permissions for this to work.\n\n"
   + "`\!yeet say` Says a thing.\n\n"
   + "`\!yeet shitpost` Markov chain for the Text Channel.\n\n"
@@ -36,12 +31,6 @@ export function runCommand(message: Message, command: string, args?: string) {
       break
     case 'help':
       say(message, HELP)
-      break
-    case 'learn':
-      learn(message, args)
-      break
-    case 'meme':
-      meme(message, args)
       break
     case 'nick':
       message.guild.members.get(client.user.id).setNickname(args ? args : 'YeetBot')
@@ -81,45 +70,6 @@ async function crypto(message: Message, currency?: string) {
   } catch(e) {
     console.error(e)
     say(message, "Uh oh... I had some trouble grabbing the info, try again later!")   
-  }
-}
-
-function learn(message: Message, args?: string) {
-  if (!args) {
-    message.channel.send("Teach me to listen for phrases like this:\n\n!yeet learn \"Sheldor\" \"Bazinga Zimbabwe\"")
-    return
-  }
-
-  // Regex out the quotes to learn
-  let newPhrase = args.match(/("([^"]|"")*")/g)
-
-  if (!newPhrase) {
-    message.channel.send("Teach me to listen for phrases like this:\n\n`!yeet learn \"Sheldor\", \"Bazinga Zimbabwe\"`")
-  } else if (newPhrase.length !== 2) {
-    message.channel.send("You need to send me a **phrase** and a **response**...")
-  } else {
-    learnPhrase(newPhrase[0].substring(1, newPhrase[0].length - 1), newPhrase[1].substring(1, newPhrase[1].length - 1))
-    say(message, DAB)
-  }
-}
-
-async function meme(message: Message, args?: string) {
-  if (!args) {
-    try {
-      let meme = await getMeme()
-      message.channel.send(`Submitted by ${meme.author}`, {embed: {image: {url: meme.url}}})
-    } catch(e) {
-      console.error(e)
-      say(message, "Memes are hard right now, try again later")
-    }
-  } else {
-    console.log(`Adding meme '${args}' by ${message.author.toString()}`)
-    if (isUrl(args)) {
-      addMeme(message.author.toString(), args)
-      say(message, DAB)
-    } else {
-      say(message, "That isn't a valid URL...")
-    }
   }
 }
 
--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const COMMIT_WEBHOOK = process.env.COMMIT_WEBHOOK || 'https://discordapp.com/api/webhooks/not-a-real-webhook/github'
 export const DEBUG = process.env.DEBUG ? true : false
-export const DISCORD_TOKEN = process.env.DISCORD_TOKEN || 'dev'
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN || 'NDIzMzAzMDQxNDA0MzcwOTUw.Dol1jQ.A91sGCqtoCO0xpY5oVu7hZALWNg'
 export const PORT = process.env.PORT || 3000
 export const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
